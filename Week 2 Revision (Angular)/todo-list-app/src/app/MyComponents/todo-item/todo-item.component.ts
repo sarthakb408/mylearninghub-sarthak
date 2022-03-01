c88cb005aebfd9b21b569b7101fa79bbb1fdab37
sarthakb408 <sarthakbhadiyadara408@gmail.com>
2022-03-02 02:58:52 +0530

    Adding files of todo list app

---
+++ Week 2 Revision (Angular)/todo-list-app/src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: any;
+  @Input() i: any;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onClick(todo: Todo) {
+    this.todoDelete.emit(todo);
+  }
+  onCheckboxClick(todo: any) {
+    this.todoCheckbox.emit(todo);
+  }
+}
