d1c8b4435364ea1f3b1447cb5b7e1eb296236b8b
sarthakb408 <sarthakbhadiyadara408@gmail.com>
2022-03-06 18:08:37 +0530

    Adding files of flight booking system

--- Week 3 Revision (Angular)/flight-booking-system/src/app/admin-login/admin-login.component.ts
+++ Week 3 Revision (Angular)/flight-booking-system/src/app/admin-login/admin-login.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AdminLoginComponent implements OnInit {
 
   constructor() { }
-
+  myForm:any;
   ngOnInit(): void {
   }
 
--- Week 3 Revision (Angular)/flight-booking-system/src/app/app-routing.module.ts
+++ Week 3 Revision (Angular)/flight-booking-system/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { AddFlightComponent } from './add-flight/add-flight.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { BookFlightComponent } from './book-flight/book-flight.component';
 import { HomeComponent } from './home/home.component';
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path:'book-flight',component:BookFlightComponent},
   {path:'add-flight',component:AddFlightComponent},
   {path:'aboutus',component:AboutusComponent},
+  {path:'admin-login',component:AdminLoginComponent},
 ];
 
 @NgModule({
